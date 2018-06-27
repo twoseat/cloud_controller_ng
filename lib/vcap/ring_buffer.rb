4246b56a1d3012b570501ffc65f5264f05d61115
Chris Selzo <cselzo@pivotal.io>
2018-06-27 14:50:03 -0700

    VCAP::RingBuffer -> ::RingBuffer

--- lib/vcap/ring_buffer.rb
+++ lib/vcap/ring_buffer.rb
@@ -1,16 +1,14 @@
-module VCAP
-  class RingBuffer < Array
-    attr_reader :max_size
+class RingBuffer < Array
+  attr_reader :max_size
 
-    def initialize(max_size)
-      @max_size = max_size
-    end
-
-    def push(item)
-      super
-      self.shift if size > @max_size
-    end
-
-    alias_method :<<, :push
+  def initialize(max_size)
+    @max_size = max_size
   end
+
+  def push(item)
+    super
+    self.shift if size > @max_size
+  end
+
+  alias_method :<<, :push
 end
