7ac4603fbc72559c4755ee90817175aea7853911
Eli Wrenn <ewrenn@pivotal.io>
2018-07-25 15:33:56 -0700

    Drop these files
    Signed-off-by: Eric Promislow <eric.promislow@suse.com>

--- lib/logcache/logcache.rb
+++
@@ -1,8 +0,0 @@
-$LOAD_PATH.unshift(File.expand_path('../models', __FILE__))
-
-require 'logcache/models/envelope.pb'
-require 'logcache/models/error.pb'
-require 'logcache/models/http.pb'
-require 'logcache/models/log.pb'
-require 'logcache/models/metric.pb'
-require 'logcache/models/uuid.pb'
