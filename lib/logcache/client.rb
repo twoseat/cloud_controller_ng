9172fa817dbe8c831df8b7e97f40f3ad3eef8cca
Eric Promislow <eric.promislow@suse.com>
2018-07-25 17:04:04 -0700

    Add a new egress class to work with log-cache
    * This is broken because the envelope object's use of
      "loggregator" conflicts with the core Loggregator class
    
    [#159042437]
    
    Signed-off-by: Eli Wrenn <ewrenn@pivotal.io>

---
+++ lib/logcache/client.rb
@@ -0,0 +1,113 @@
+require 'multipart_parser/reader'
+require 'logcache/logcache_egress_services_pb'
+
+module Logcache
+  class Client
+    class Error < StandardError
+    end
+
+    def initialize(host:, port:, client_ca_path:, client_cert_path:, client_key_path:)
+      client_ca = File.open(client_ca_path).read
+      client_key = File.open(client_key_path).read
+      client_cert = File.open(client_cert_path).read
+
+      @service = Logcache::V1::Egress::Stub.new(
+        "#{host}:#{port}",
+        GRPC::Core::ChannelCredentials.new(client_ca, client_key, client_cert)
+      )
+    end
+
+    def container_metrics(auth_token: nil, app_guid:)
+      response = with_request_error_handling do
+        service.read(build_read_request(app_guid))
+      end
+
+      validate_status!(response: response, statuses: [200])
+
+      envelopes = []
+      boundary  = extract_boundary!(response.contenttype)
+      parser    = MultipartParser.new(body: response.body, boundary: boundary)
+      until (next_part = parser.next_part).nil?
+        envelopes << protobuf_decode!(next_part, Models::Envelope)
+      end
+      envelopes
+    end
+
+    def with_request_error_handling(&blk)
+      tries ||= 3
+      yield
+    rescue => e
+      retry unless (tries -= 1).zero?
+      raise RequestError.new(e.message)
+    end
+
+    private
+
+    def build_read_request(source_id)
+      Logcache::V1::ReadRequest.new(
+        {
+          source_id: source_id
+        }
+      )
+    end
+
+    def extract_boundary!(content_type)
+      match_data = BOUNDARY_REGEXP.match(content_type)
+      raise ResponseError.new('failed to find multipart boundary in Content-Type header') if match_data.nil?
+
+      match_data.captures.first
+    end
+
+    def validate_status!(response:, statuses:)
+      raise ResponseError.new("failed with status: #{response.status}, body: #{response.body}") unless statuses.include?(response.status)
+    end
+
+    def protobuf_decode!(message, protobuf_decoder)
+      protobuf_decoder.decode(message)
+    rescue => e
+      raise DecodeError.new(e.message)
+    end
+
+    class MultipartParser
+      NEW_LINE = "\r\n".freeze
+
+      def initialize(body:, boundary:)
+        @body     = body
+        @boundary = boundary
+      end
+
+      def next_part
+        @chunks ||= parse(@body, @boundary)
+        @chunks.next
+      rescue StopIteration, ParseError
+        nil
+      end
+
+      private
+
+      def parse(body, boundary)
+        parts = []
+
+        reader = ::MultipartParser::Reader.new(boundary)
+
+        reader.on_part do |part|
+          p = []
+
+          part.on_data do |partial_data|
+            p << partial_data
+          end
+
+          parts << p
+        end
+
+        reader.write body
+
+        unless reader.ended?
+          raise ParseError.new('truncated multipart message')
+        end
+
+        parts.map(&:join).to_enum
+      end
+    end
+  end
+end
